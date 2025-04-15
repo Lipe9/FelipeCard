712a96da188b24953c029680fa4c2d0aa3b024e2
Felipe ツ <antonio.pereira312@aluno.ce.gov.br>
2025-04-15 16:44:53 -0300

    Atualizar o javascript.js

--- javascript.js
+++ javascript.js
@@ -1,18 +1,42 @@
+var btn = document.getElementById('read-btn');
+var btn2 = document.getElementById('read-btn2');
+
+btn.addEventListener('click', () => {
+    var face = document.querySelector('.face');
+    face.classList.toggle('active');
+
+    if (face.classList.contains('active')) {
+        return btn.textContent = 'Saiba Mais';
+    }
+
+    btn.textContent = 'Voltar';
+});
+
+btn2.addEventListener('click', () => {
+    var face = document.querySelector('.face');
+    face.classList.toggle('active');
+
+    if (face.classList.contains('active2')) {
+        return btn2.textContent = 'Saiba Mais';
+    }
+
+    btn2.textContent = 'Voltar';
+});
 
 function showBack() {
-    const card = document.getElementById('card');
-    card.classList.remove('show-courses');
-    card.classList.add('show-back');
+  const card = document.getElementById('card');
+  card.classList.remove('show-courses');
+  card.classList.add('show-back');
 }
 
 function showCourses() {
-    const card = document.getElementById('card');
-    card.classList.remove('show-back');
-    card.classList.add('show-courses');
+  const card = document.getElementById('card');
+  card.classList.remove('show-back');
+  card.classList.add('show-courses');
 }
 
 function showFront() {
-    const card = document.getElementById('card');
-    card.classList.remove('show-back');
-    card.classList.remove('show-courses');
+  const card = document.getElementById('card');
+  card.classList.remove('show-back');
+  card.classList.remove('show-courses');
 }
