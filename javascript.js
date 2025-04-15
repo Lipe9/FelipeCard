5c6f341c12ea8275dac25a109fc6bbf6fb0c6379
Felipe ツ <antonio.pereira312@aluno.ce.gov.br>
2025-04-15 16:47:53 -0300

    Atualizar o javascript.js

--- javascript.js
+++ javascript.js
@@ -22,21 +22,3 @@ btn2.addEventListener('click', () => {
 
     btn2.textContent = 'Voltar';
 });
-
-function showBack() {
-  const card = document.getElementById('card');
-  card.classList.remove('show-courses');
-  card.classList.add('show-back');
-}
-
-function showCourses() {
-  const card = document.getElementById('card');
-  card.classList.remove('show-back');
-  card.classList.add('show-courses');
-}
-
-function showFront() {
-  const card = document.getElementById('card');
-  card.classList.remove('show-back');
-  card.classList.remove('show-courses');
-}
