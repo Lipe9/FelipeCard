4b6f377646ee7832c88f28ac9eda9865034ff8f9
Felipe ツ <antonio.pereira312@aluno.ce.gov.br>
2025-04-15 17:33:21 -0300

    Atualizar o javascript.js

--- javascript.js
+++ javascript.js
@@ -1,36 +1,24 @@
-var btn = document.getElementById('read-btn');
-var btn2 = document.getElementById('read-btn2');
-var btn3 = document.getElementById('read-btn3');
+  const face = document.querySelector('.face');
+  const btn = document.getElementById('read-btn');
+  const btn2 = document.getElementById('read-btn2');
+  const btn3 = document.getElementById('read-btn3');
+  const btn4 = document.getElementById('read-btn4');
 
-btn.addEventListener('click', () => {
-    var face = document.querySelector('.face');
-    face.classList.toggle('active');
+  btn.addEventListener('click', () => {
+      face.classList.toggle('active');
+      face.classList.remove('active3');
+  });
 
-    if (face.classList.contains('active')) {
-        return btn.textContent = 'Saiba Mais';
-    }
+  btn2.addEventListener('click', () => {
+      face.classList.toggle('active');
+      face.classList.remove('active3');
+  });
 
-    btn.textContent = 'Voltar';
-});
+  btn3.addEventListener('click', () => {
+      face.classList.add('active3');
+      face.classList.remove('active');
+  });
 
-btn2.addEventListener('click', () => {
-    var face = document.querySelector('.face');
-    face.classList.toggle('active');
-
-    if (face.classList.contains('active2')) {
-        return btn2.textContent = 'Saiba Mais';
-    }
-
-    btn2.textContent = 'Voltar';
-});
-
-btn3.addEventListener('click', () => {
-    var curso = document.querySelector('.curso');
-    curso.classList.toggle('active3');
-
-    if (curso.classList.contains('active3')) {
-        return btn3.textContent = 'Cursos';
-    }
-
-    btn3.textContent = 'Voltar';
-});
+  btn4.addEventListener('click', () => {
+      face.classList.remove('active3');
+  });
